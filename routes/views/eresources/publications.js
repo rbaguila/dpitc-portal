0e90ef3f34e53abcaaf21762e49e5aba63cf3673
Monina Carandang <reactor04@gmail.com>
2017-03-30 15:54:30 +0800

    Moved publications to its own page. Eresources is now a hub of sorts

---
+++ routes/views/eresources/publications.js
@@ -0,0 +1,38 @@
+var keystone = require('keystone');
+
+exports = module.exports = function (req, res) {
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+  locals.section = 'eresources';
+
+  // console.log(req._parsedUrl.query);
+
+  view.query('industries', keystone.list('Industry').model.find())
+  view.query('sectors', keystone.list('Sector').model.find())
+  view.query('commodities', keystone.list('Commodity').model.find())
+
+
+  var viewStyle = req.query.view == undefined ? 'grid' : req.query.view
+  var searchTerm = req.query.term
+  var searchCategory = req.query.category
+
+
+  view.query('publications', keystone.list('Publication').model.find())
+
+
+  var pageData = {
+    loginRedirect: '/eresources',
+    breadcrumbs: [
+      { text: 'E Resources', link: '/eresources'},
+      { text: 'Publications', link: '/publications'},
+    ]
+  }
+
+  if (viewStyle == 'list') {
+    //render list layout
+    view.render('eresources/publications-list', pageData);
+  } else {
+    //render grid layout by default
+    view.render('eresources/publications-grid', pageData);
+  }
+}
