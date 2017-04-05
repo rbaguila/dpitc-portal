1f33c920a0378f3045d525d69f7729720fac41d0
Monina Carandang <reactor04@gmail.com>
2017-04-05 14:06:42 +0800

    Added badges to top categories

--- routes/views/eresources/eresources.js
+++ routes/views/eresources/eresources.js
@@ -1,11 +1,14 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res);
   var locals = res.locals;
   locals.section = 'eresources';
 
-  view.query('publications', keystone.list('Publication').model.find());
+  locals.data = {
+    industries: []
+  }
 
   var pageData = {
     loginRedirect: '/eresources',
@@ -13,6 +16,40 @@ exports = module.exports = function(req, res) {
       { text: 'E Resources', link: '/eresources'},
     ]
   }
+
+  view.query('publications', keystone.list('Publication').model.find().limit(10));
+  view.query('links', keystone.list('Link').model.find().limit(3))
+
+  view.on('init', function(next) {
+    keystone.list('Industry').model.find().exec(function(err, result) {
+      if (err) {
+        return next(err);
+      }
+      locals.data.industries = result;
+      // next(err)
+      async.each(locals.data.industries, function(industry, next) {
+        keystone.list('Publication').model.count().where('industry').in([industry.id]).exec(function(err, count) {
+          industry.publicationCount = count;
+          next(err);
+        });
+      }, function(err) {
+          next(err);
+        }
+      );
+    });
+
+  });
+
+  // async.each(locals.data.categories, function(category, next) {
+  //       keystone.list('Products').model.count().where('categories').in([category.id]).exec(function(err, count) {
+  //         category.postCount = count;
+  //         next(err);
+  //       });
+
+  //     }, function(err) {
+  //       next(err);
+  //     });
+
 
   view.render('eresources/eresources', pageData);
 }
