4ee5223af0fef3022e660d8479b93b5d18faf791
Monina Carandang <reactor04@gmail.com>
2017-04-05 16:26:16 +0800

    Started on individual publication page

---
+++ routes/views/eresources/publication.js
@@ -0,0 +1,26 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+  locals.section = 'eresources';
+
+  locals.data = {
+
+  }
+
+  var pageData = {
+    loginRedirect: '/eresources/publications',
+    breadcrumbs: [
+      { text: 'E Resources', link: '/eresources'},
+      { text: 'Publications', link: '/publications'},
+    ]
+  }
+
+  var pubId = req.params.publication
+
+  view.query('publication', keystone.list('Publication').model.findOne({_id: pubId}));
+
+  view.render('eresources/publication', pageData);
+
+}
