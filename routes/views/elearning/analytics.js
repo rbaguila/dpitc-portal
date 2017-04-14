677108e1a99a5ff22f9f6a522dca47ced5ba1e13
dsdinglasan <Veigny@gmail.com>
2017-04-15 00:05:54 +0800

    Added the dashboard for analytics

--- routes/views/elearning/analytics.js
+++ routes/views/elearning/analytics.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function(req, res){
     
@@ -10,18 +11,16 @@ exports = module.exports = function(req, res){
     locals.section = 'analytics';
 
     locals.data = {
-        maxReactionsLearningObject: [],
-        minReactionsLearningObject: [],
-        recentLOComment1: [],
-        recentLOComment2: [],
-        recentLOView1: [],
-        recentLOView2: [],
+        mostPopularLO: [],
+        mostCommentedLO: [],
+        mostReactedLO: [],
+        numCourses: [],
+        numLO: [],
+        numUsers: []
     };
-    
-    var recentComment1;
-    var recentComment2;
-    var recentView1;
-    var recentView2;
+
+    var tempLearningObjects = [];
+    var tempLearningObjects2 = [];
 
     view.on('init', function(next){
 
@@ -38,103 +37,103 @@ exports = module.exports = function(req, res){
                 results.sort(function(a,b){
                     return parseFloat(b.reactions) - parseFloat(a.reactions);
                 });
-                locals.data.maxReactionsLearningObject = results[0];
-                locals.data.minReactionsLearningObject = results[(results.length-1)];
+                locals.data.mostReactedLO = results[0];
             }
             next(err);
         });
     });
 
-    //GET 2 Sample learning Object and show the graph for the number of comments
-    //get the learning object wtih the most recent comment
-    //TO DO
-    //get the LO with the highest and lowest number of comments or LOs recently created instead of getting the learning object with the most recent comment
-
     view.on('init', function(next){
 
-        var q = keystone.list('LOComment').model.find().sort('-dateCreated');
+        var q = keystone.list('LearningObject').model.find();
 
         q.exec(function(err, results){
-            if(results!=null){
-                recentComment1 = results[0];
-                recentComment2 = results[1];
+
+            if (err || !results.length) {
+                return next(err);
             }
-            next(err);
+            tempLearningObjects = results;
+
+            async.each(tempLearningObjects, function (learningObject, next) {
+                keystone.list('LOView').model.count().where('learningObject', learningObject._id).exec(function (err, count) {
+                    learningObject.viewCount = count;
+                    next(err);
+                });
+            }, function (err) {
+                next(err);
+            });
         });
     });
 
     view.on('init', function(next){
-        if(recentComment1!=null){
-            var q = keystone.list('LearningObject').model.find().where('_id', recentComment1.learningObject);
 
-            q.exec(function(err, result){
-                locals.data.recentLOComment1 = result[0];
-                next(err);
-            });
-        }
-        else{
-            next();
-        }
-    });
-
-    view.on('init', function(next){
-        if(recentComment2!=null){
-            var q = keystone.list('LearningObject').model.find().where('_id', recentComment2.learningObject);
-
-            q.exec(function(err, result){
-                locals.data.recentLOComment2 = result[0];
-                next(err);
-            });
-        }
-        else{
-            next();
-        }
-    });
-
-    //GET 2 Sample learning Object and show the graph for the number of views
-    //get the learning object wtih the most recent views
-    //TO DO
-    //get the LO with the highest and lowest number of views or LOs recently created instead of getting the learning object with the most recent views
-
-    view.on('init', function(next){
-
-        var q = keystone.list('LOView').model.find().sort('-dateViewed');
+        var q = keystone.list('LearningObject').model.find();
 
         q.exec(function(err, results){
-            if(results != null){
-                recentView1 = results[0];
-                recentView2 = results[1];
+
+            if (err || !results.length) {
+                return next(err);
             }
+            tempLearningObjects2 = results;
+
+            async.each(tempLearningObjects2, function (learningObject, next) {
+                keystone.list('LOComment').model.count().where('learningObject', learningObject._id).exec(function (err, count) {
+                    learningObject.commentCount = count;
+                    next(err);
+                });
+            }, function (err) {
+                next(err);
+            });
+        });
+    });
+
+    view.on('init', function(next){
+        tempLearningObjects.sort(function(a,b){
+          return parseFloat(b.viewCount) - parseFloat(a.viewCount);
+        });
+        locals.data.mostPopularLO = tempLearningObjects[0];
+
+        tempLearningObjects2.sort(function(a,b){
+          return parseFloat(b.commentCount) - parseFloat(a.commentCount);
+        });
+        locals.data.mostCommentedLO = tempLearningObjects2[0];
+        next();
+    });
+
+    view.on('init', function(next){
+        var q = keystone.list('Course').model.count();
+
+        q.exec(function(err, count){
+            if (err || count==null) {
+                return next(err);
+            }
+            locals.data.numCourses = count;
             next(err);
         });
     });
 
     view.on('init', function(next){
-        if(recentView1!=null){
-            var q = keystone.list('LearningObject').model.find().where('_id', recentView1.learningObject);
+        var q = keystone.list('LearningObject').model.count();
 
-            q.exec(function(err, result){
-                locals.data.recentLOView1 = result[0];
-                next(err);
-            });
-        }
-        else{
-            next();
-        }
+        q.exec(function(err, count){
+            if (err || count==null) {
+                return next(err);
+            }
+            locals.data.numLO = count;
+            next(err);
+        });
     });
 
     view.on('init', function(next){
-        if(recentView2!=null){
-            var q = keystone.list('LearningObject').model.find().where('_id', recentView2.learningObject);
+        var q = keystone.list('User').model.count();
 
-            q.exec(function(err, result){
-                locals.data.recentLOView2 = result[0];
-                next(err);
-            });
-        }
-        else{
-            next();
-        }
+        q.exec(function(err, count){
+            if (err || count==null) {
+                return next(err);
+            }
+            locals.data.numUsers = count;
+            next(err);
+        });
     });
 
     //Render the view
