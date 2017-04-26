7a23de04a6a500d71fd7bcbd0b5b20cf9d51412d
aacarrasco <aacarrasco@up.edu.ph>
2017-04-27 04:14:52 +0800

    edited LOView model

--- models/elearning/reactions/LOView.js
+++ models/elearning/reactions/LOView.js
@@ -4,20 +4,22 @@ var Types = keystone.Field.Types;
 var LOView = new keystone.List('LOView', {
 	track: true,
 	nocreate: true,
-	hidden: true
+	//hidden: true
 })
 
 LOView.add({
 	user: {
 		type: Types.Relationship,
-		ref: 'User'
+		ref: 'User',
+		noedit: true,
 	},
 	learningObject: {
 		type: Types.Relationship,
-		ref: 'LearningObject'
+		ref: 'LearningObject',
+		noedit: true,
 	},
 	dateViewed: {//change this before deplyoning, use createdAt instead
-		type: Types.Date, 
+		type: Types.Datetime, 
 		index: true, 
 		default: Date.now 
 	},
@@ -27,6 +29,7 @@ LOView.add({
 	}
 });
 
-LOView.defaultColumns = 'LUser';
+LOView.defaultSort = '-dateViewed';
+LOView.defaultColumns = 'learningObject, user, dateViewed, createdAt';
 
 LOView.register();
