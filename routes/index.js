427b45d4c95320e49ea2f411015f7d825efbc85b
aacarrasco <aacarrasco@up.edu.ph>
2017-05-07 04:59:21 +0800

    refactored and added searcch functionality to courseList

--- routes/index.js
+++ routes/index.js
@@ -61,7 +61,7 @@ exports = module.exports = function (app) {
 
 	// Elearning Routes
 	app.get('/elearning', routes.elearningViews.content.elearning);
-	app.get('/elearning/courses?', routes.elearningViews.courseList);
+	app.get('/elearning/courses?', routes.elearningViews.content.courseList);
 	app.get('/elearning/course/:courseslug?', routes.elearningViews.courseOutline);
 	app.get('/elearning/:courseslug/learning-object/:learningobjectslug?', routes.elearningViews.learningObject);
 	app.post('/elearning/:courseslug/learning-object/:learningobjectslug', routes.elearningViews.learningObject);
