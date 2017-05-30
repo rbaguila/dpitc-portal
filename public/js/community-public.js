51c3ec35baae2389c97477e48e4393da1fa90e75
Simon Barrameda <barramedasimon321@gmail.com>
2017-05-30 23:32:44 +0800

    Added image placeholders for null values

--- public/js/community-public.js
+++ public/js/community-public.js
@@ -56,6 +56,8 @@ $(document).ready(function() {
       var id = $(this).attr('data-auth');
       var user = {};
 
+      $(this).attr('src', 'http://placehold.it/100x100');
+      
       $.ajax({
         type: 'GET',
         url: host+'api/users/'+id,
