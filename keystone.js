bc9b20028651bd5c993f06a2872966ea8670e41f
Roinand Aguila <rbaguila1@up.edu.ph>
2017-05-30 13:03:42 +0800

    Changed the brand of the site

--- keystone.js
+++ keystone.js
@@ -11,7 +11,7 @@ var keystone = require('keystone');
 
 keystone.init({
 	'name': 'DPITC',
-	'brand': 'DPITC',
+	'brand': 'DPITC Content Management System',
 
 	'less': 'public',
 	'static': 'public',
